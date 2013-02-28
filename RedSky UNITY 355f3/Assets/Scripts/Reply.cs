089e18cdaa1cf0fb914fe285da58fc4f8dece1ee
wtarr <william.projects.2012@gmail.com>
2013-02-28 20:57:04 +0000

    Add ping and reply script

---
+++ RedSky UNITY 355f3/Assets/Scripts/Reply.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+
+public class Reply : MonoBehaviour {
+	
+	bool reply;
+	float tick;
+	Vector3 scale;
+	GameObject replyToSender;
+	string message;
+	public Material transparent;
+	
+	// Use this for initialization
+	void Start () {
+		reply = false;
+		tick = 1;
+		scale = new Vector3(10, 10, 10);
+		message = string.Empty;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (reply)
+		{
+				reply = false;
+			
+				replyToSender = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				
+				replyToSender.AddComponent<Rigidbody>();
+											
+				replyToSender.name = message;
+			
+				replyToSender.transform.position = transform.position;
+				
+				Rigidbody rb = replyToSender.GetComponent<Rigidbody>();
+				
+				rb.useGravity = false;
+				rb.drag = 0f;
+				rb.angularDrag = 0f;				
+				
+				SphereCollider p = replyToSender.GetComponent<SphereCollider>();
+				
+				p.isTrigger = true;
+				
+				Renderer r = replyToSender.GetComponent<Renderer>();
+				
+				r.material = transparent;
+			
+				//r.enabled = false;
+				
+				//p.radius = 10;
+				
+				
+		}
+		
+		if (replyToSender != null)
+		{			
+			replyToSender.transform.position = transform.position;
+			replyToSender.transform.localScale= scale * tick;
+			tick++;			
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name.Contains("ping") && !other.gameObject.name.Contains("reply"))
+		{
+			Debug.Log("Pinged");	
+			
+			if (message == string.Empty)
+				message = "reply"; //  + gameObject.name + " to " + other.gameObject.name + " reliers coords " + gameObject.transform.position.ToString();
+			
+			reply = true;
+		
+			Destroy(other.gameObject);
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
